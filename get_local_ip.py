c462652f277b73d75559a2111bf7bdd6ba4fcfc8
Roman Malkevich <pomannero52@gmail.com>
2022-03-09 17:46:32 +0200

    -->

---
+++ get_local_ip.py
@@ -0,0 +1,6 @@
+from re import L
+import socket
+from threading import local
+hostname = socket.gethostname();
+local_ip = socket.gethostbyname(hostname);
+print(f"HOSTname: {hostname}\nIP address: {local_ip}");
